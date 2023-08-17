7dd21ba6ce350d53aa4439c9e8b9b95cd330c03f
Eduardo9494 <1433341@senati.pe>
2023-08-17 10:26:10 -0500

    primero declaramos y asignamos las varibles de nuestra suma y resta

---
+++ SumaResta/src/main/java/com/mycompany/sumaresta/SumaResta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumaresta;
+
+/**
+ *
+ * @author Alumno
+ */
+public class SumaResta {
+
+    public static void main(String[] args) {
+        System.out.println("suma y resta ");
+        int a = 5;
+        int b = 3;
+        
+        
+    }
+}
